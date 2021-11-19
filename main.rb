cce7a471f3fdc18ec396d5cbd0d59237cd08411f
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-19 21:21:49 +0300

    each_slice(2)

--- main.rb
+++ main.rb
@@ -1,5 +1,31 @@
 strings = File.readlines('input_files/test.txt')
           .map(&:chomp).reject!(&:empty?).uniq
 
-p strings
+# p strings.map! { |string| string.split('://') }
+# p (strings.map do |arr|
+#     arr.map { |string| string.split('/') }
+#   end)
+
+# => 
+# [["https:", "", "www.dns-shop.ru", ", http:", "", "dns-shop.ru", "?city=abinsk"], ...]]
+p strings.map! { |string| string.split('/') }
+puts '='*40
+
+# [["https:", "", "www.dns-shop.ru", ", http:", "", "dns-shop.ru", "?city=abinsk"], ...]]
+p strings.map! { |arr| arr.reject!(&:empty?) }
+puts '='*40
+
+# [["https:", "www.dns-shop.ru", "http:", "dns-shop.ru", "?city=abinsk"], ...]]
+p (strings.map! do |arr|
+    arr.map { |string| string.gsub(', ', '') }
+  end)
+
+# [[["https:", "www.dns-shop.ru"], ["http:", "dns-shop.ru"], ["?city=abinsk"]], ...]]]
+puts '='*40
+p (strings.map! do |arr|
+    arr.each_slice(2).to_a
+  end)
+
+
+
 p strings.count
