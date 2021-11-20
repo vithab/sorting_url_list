15cc072c81ecc5ff818359c3a175062146733f7c
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-20 18:52:01 +0300

    refactoring

--- main.rb
+++ main.rb
@@ -1,44 +1,11 @@
-strings = File.readlines('input_files/test.txt')
+strings = File.readlines('input_files/input_strings.txt')
           .map(&:chomp).reject!(&:empty?).uniq
 
-# p strings.map! { |string| string.split('://') }
-# p (strings.map do |arr|
-#     arr.map { |string| string.split('/') }
-#   end)
+urls = strings.map { |string| string.split(', ') }.flatten.uniq.sort
 
-# => 
-# [["https:", "", "www.dns-shop.ru", ", http:", "", "dns-shop.ru", "?city=abinsk"], ...]]
-p strings.map! { |string| string.split('/') }
 puts '='*40
 
-# [["https:", "", "www.dns-shop.ru", ", http:", "", "dns-shop.ru", "?city=abinsk"], ...]]
-p strings.map! { |arr| arr.reject!(&:empty?) }
+urls.each { |url| p url }
+
 puts '='*40
-
-# [["https:", "www.dns-shop.ru", "http:", "dns-shop.ru", "?city=abinsk"], ...]]
-p (strings.map! do |arr|
-    arr.map { |string| string.gsub(', ', '') }
-  end)
-
-# [[["https:", "www.dns-shop.ru"], ["http:", "dns-shop.ru"], ["?city=abinsk"]], ...]]]
-puts '='*40
-p (strings.map! do |arr|
-    arr.each_slice(2).to_a
-  end)
-
-uniq_urls = []
-
-strings.each do |arr|
-  arr.each do |arr|
-    # p arr
-    if arr[0].include?('https:') || arr[0].include?('http:')
-      uniq_urls << arr.join('//')
-    else
-      next
-    end
-  end
-end
-
-p uniq_urls.uniq!
-uniq_urls.each { |url| p url }
-p uniq_urls.count
+puts "Всего уникальных урлов: #{urls.count}"
